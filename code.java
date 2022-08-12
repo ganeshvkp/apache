de379052346eb57ca85b5c2cf06d6831524d9c10
ganesh <ganesh56563gmail.com>
2022-08-12 07:37:45 +0000

    3rd

--- code.java
+++ code.java
@@ -1,7 +1,7 @@
 class Main {
   public static void main(String[] args) {
 
-    String str = "Radar", reverseStr = "";
+    String str = "madam", reverseStr = "";
     
     int strLength = str.length();
 
